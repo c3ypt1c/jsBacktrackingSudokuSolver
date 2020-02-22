579773731a4ce8f174affb46343070feccf57325
C3ypt1c <c3ypt1c@gmail.com>
2020-02-22 19:07:47 +0000

    Added finding possible values

--- solver.js
+++ solver.js
@@ -1,3 +1,63 @@
+var possible = [];
+
+Array.prototype.in = function(item) {
+	return this.indexOf(item) != -1;
+}
+
+function findPossibleRow(row) {
+	let currentPossible = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	for (var item = row.length - 1; item >= 0; item--) {
+		let elem = row[item];
+		if(elem == 0) continue;
+
+		let index = currentPossible.indexOf(elem);
+		if(index != -1) currentPossible.splice(index,1);
+		else return false;
+	}
+	return currentPossible;
+}
+
+function findPossibleCol(grid, col) {
+	let rows = [];
+	for (var row = grid.length - 1; row >= 0; row--) rows.push(grid[row][col]);
+
+	return findPossibleRow(rows);
+}
+
+function findPossibleQua(grid, row, col) {
+	let elem = [];
+	for (var x = row * 3; x < row * 3 + 3; x++) {
+		for (var y = col * 3; y < col * 3 + 3; y++) {
+			elem.push(grid[y][x]);
+		}
+	}
+	return findPossibleRow(elem);
+}
+
+function findPossibleSummary(grid, row, col) {
+	let rowVals = findPossibleRow(grid[row]);
+	console.log(rowVals);
+	let colVals = findPossibleCol(grid, col);
+	console.log(colVals);
+	let quaVals = findPossibleQua(grid, Math.floor(row / 3), Math.floor(col / 3));
+	console.log(quaVals);
+	let currentPossible = [];
+
+	for (var i = 1; i < 10; i++) {
+		if(rowVals.in(i) && colVals.in(i) && quaVals.in(i)) currentPossible.push(i);
+	}
+
+	return currentPossible;
+}
+
+function findAllPossible() {
+
+}
+
+function findPossible(row, col) {
+	let grid = getGrid();
+}
+
 function $(id) {
 	return document.getElementById(id);
 }
@@ -13,7 +73,7 @@ function genSelect() {
 
 	let wrapper = document.createElement("div");
 	wrapper.classList.add("wrapper");
-	wrapper.appendChild(select)
+	wrapper.appendChild(select);
 	return wrapper;
 }
 
