a4b40e93080667fcba186f11e6914e009acb8b17
C3ypt1c <c3ypt1c@gmail.com>
2020-02-24 00:30:15 +0000

    Manages to solve a valid puzzle

--- solutionWorker.js
+++ solutionWorker.js
@@ -1,10 +1,3 @@
-possible = []
-var possible = new Array(9);
-
-for (var i = 0; i < 9; i++) {
-	possible[i] = new Array(9); //populate arrays
-}
-
 Array.prototype.in = function(item) {
 	return this.indexOf(item) != -1;
 }
@@ -41,11 +34,8 @@ function findPossibleQua(grid, row, col) {
 
 function findPossibleSummary(grid, row, col) {
 	let rowVals = findPossibleRow(grid[row]);
-	//console.log(rowVals);
 	let colVals = findPossibleCol(grid, col);
-	//console.log(colVals);
 	let quaVals = findPossibleQua(grid, Math.floor(row / 3), Math.floor(col / 3));
-	//console.log(quaVals);
 	let currentPossible = [];
 
 	for (var i = 1; i < 10; i++) {
@@ -55,21 +45,71 @@ function findPossibleSummary(grid, row, col) {
 	return currentPossible;
 }
 
-function findAllPossible(grid) {
+function findAllPossibleLocal(grid) {
+	possibleLocal = []
+	var possible = new Array(9);
+	for (var i = 0; i < 9; i++) {
+		possibleLocal[i] = new Array(9); //populate arrays
+	}
+
 	for (var row = 0; row < grid.length; row++) {
 		for (var col = 0; col < grid[row].length; col++) {
-			possible[row][col] = findPossibleSummary(grid, row, col);
-			//grid[row][col]
+			if (grid[row][col] != 0) possibleLocal[row][col] = findPossibleSummary(grid, row, col);
+			else possibleLocal[row][col] = [];
 		}
 	}
+	return possibleLocal;
 }
 
-function solve(e) {
-	let grid = e.data;
-	findAllPossible(grid);
+function checkSolved(grid) {
+	for (var row = grid.length - 1; row >= 0; row--) {
+		for (var col = grid[row].length - 1; col >= 0; col--) {
+			if(grid[row][col] == 0) return false;
+			let tmp = grid[row][col];
+			grid[row][col] = 0;
+			let tmpResults = findPossibleSummary(grid, row, col);
+			grid[row][col] = tmp;
+			if(tmpResults.length != 1) return false;
+		}
+	}
+	return true;
+}
 
-	do { //fist, reduce
-		//console.log(grid);
+function backtracking(grid, iteration) {
+	//debugger;
+	let row = Math.floor(iteration/9);
+	let col = iteration % 9;
+	
+	//console.log(grid);
+	//console.log(row);
+	//console.log(col);
+	
+	if(checkSolved(grid)) return grid;
+
+	if(grid[row][col] != 0) return backtracking(grid, iteration+1);
+
+
+	let possibleLocal = findPossibleSummary(grid, row, col);
+
+	let tmp = grid[row][col];
+
+	for (var i = 0; i < possibleLocal.length; i++) {
+		grid[row][col] = possibleLocal[i];
+		if(checkSolved(grid)) return grid;
+
+		let result = backtracking(grid, iteration+1);
+		if(result !== false) return result;
+	}
+
+	grid[row][col] = tmp;
+
+	return false; //failed to solve or out of options
+}
+
+function reduce(grid) {
+	let possible = findAllPossibleLocal(grid);
+
+	do {
 		changed = false;
 		for (var row = 0; row < possible.length; row++) {
 			for (var col = 0; col < possible[row].length; col++) {
@@ -80,9 +120,17 @@ function solve(e) {
 			}
 		}
 
-		findAllPossible(grid);
+		possible = findAllPossibleLocal(grid);
 	} while (changed);
 
+	return grid;
+}
+
+function solve(e) {
+	let grid = e.data;
+	//grid = reduce(grid);
+	//debugger;
+	if(!checkSolved(grid)) grid = backtracking(grid, 0);
 	self.postMessage(grid);
 }
 
